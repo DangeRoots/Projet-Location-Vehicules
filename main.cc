9af9c9bc41781cf302242aa733a63067f2eb1556
Gilles <gilles.coulais@etu.u-bordeaux1.fr>
2012-02-12 18:00:12 +0100

    Classe CDate : modification du constructeur par défaut Location : modification de la méthode afficher() Ajout de tests dans le main.cc

--- main.cc
+++ main.cc
@@ -6,57 +6,84 @@ using namespace std;
 #include <Utilitaire.h>
 #include <Camion.h>
 #include <VP.h>
+#include <CDate.h>
+#include <Location.h>
 
 int main() {
-	Vehicule v1; // Constructeur par défaut
-	Vehicule v2("1234XY33", "Peugeot", "304", 22212);// Constructeur avec paramètres
+	// Déclarations et variables
+	// Constructeurs par défaut
+	Vehicule v1; 
+	Utilitaire u2;
+	Camion c1;
+	VP vp1;
+	Location loc1;
 	
+	// Constructeurs avec paramètres
+	Vehicule v2("1234XY33", "Peugeot", "304", 22212);
+	Utilitaire u1(10.0, "IMMAT_U1", "MARQUE_U1", "MODELE_U1", 200);
+	Camion c2(12,12,"IMMAT_CAMION2", "Marque_camion2", "Modele_camion2", 100);
+	VP vp2("0012ZZ87", "Porshe", "911 Carrera", 80000, 2 );
+	// Location(Vehicule vehicule, 
+	//			CDate dateDepart, 
+	//			CDate dateRetourPrevu, 
+	//			CDate dateRetourReel, 
+	//			int kmDepart, 
+	//			int kmRetour)
+	// Location avec Véhicule
+	Location loc2(v2, CDate(12,2,2012), CDate(17,2,2012), CDate(), 10000, -1);
+	// Location avec Utilitaire
+	Location loc3(u1, CDate(12,2,2012), CDate(17,2,2012), CDate(), 10000, -1);
+	// Location avec Camion
+	Location loc4(c2, CDate(12,2,2012), CDate(17,2,2012), CDate(), 10000, -1);
+	// Location avec VP
+	Location loc5(vp2, CDate(12,2,2012), CDate(17,2,2012), CDate(), 10000, -1);
+	
+	// Fonctions et méthodes
+	cout << "---------------------" << endl;
 	v1.setKilometrage(10);
 	v1.setImmatriculation("IMMAT_V1");
 	v1.setMarque("Renault");
 	v1.setModele("Plastique");
-	
-	cout << "---------------------" << endl;
 	cout << "Vehicule sans param" << endl;
 	v1.afficher();
-	cout << "---------------------" << endl;
 	cout << "Vehicule avec param" << endl;
 	v2.afficher();
-	
-	Utilitaire u1(10.0, "IMMAT_U1", "MARQUE_U1", "MODELE_U1", 200);
-	Utilitaire u2;
-	
 	cout << "---------------------" << endl;
 	cout << "Utilitaire avec param" << endl;
 	u1.afficher();
-	cout << "---------------------" << endl;
 	cout << "Utilitaire sans param" << endl;
 	u2.afficher();
-	
+	cout << "-- Modification du kilométrage --" << endl;
 	u2.setKilometrage(30);
 	u2.afficher();
-	
-	Camion c1;
-	Camion c2(12,12,"IMMAT_CAMION2", "Marque_camion2", "Modele_camion2", 100);
-	
-	cout << "---------------------" << endl;
-	cout << "Camion sans param" << endl;
-	c1.afficher();
 	cout << "---------------------" << endl;
 	cout << "Camion avec param" << endl;
 	c2.afficher();
+	cout << "Camion sans param" << endl;
+	c1.afficher();
+	cout << "-- Modification du poids utile --" << endl;
 	c1.setPoidsUtile(12);
 	c1.afficher();
-	
-	VP vp1;
-	VP vp2("0012ZZ87", "Porshe", "911 Carrera", 80000, 2 );
-	
 	cout << "---------------------" << endl;
 	cout << "VP sans param" << endl;
 	vp1.afficher();
 	cout << "---------------------" << endl;
 	cout << "VP avec paramètre" << endl;
 	vp2.afficher();
+	cout << "-- Modification du nombre de places --" << endl;
 	vp1.setNbPlaces(3);
 	vp1.afficher();
+	
+	// Tester l'héritage de méthode
+	// setKilometrage : classe Vehicule
+	// vp1 : classe VP
+	vp1.setKilometrage(10000);
+	cout << "-- Tests de fonctions héritées --" << endl;
+	cout << "Camion sans param" << endl;
+	c1.afficher();
+	cout << "Camion sans param" << endl;
+	c1.setKilometrage(10000);
+	c1.afficher();
+	loc2.afficher();
+	loc1.afficher();
 }
--- src/Location.cc
+++ src/Location.cc
@@ -7,15 +7,22 @@ using namespace std;
 #include <Location.h>
 
 Location::Location() {
-	
+	Vehicule veh;
+	CDate d;
+	m_vehicule = veh;
+	m_dateDepart = d;
+	m_dateRetourPrevu = d;
+	m_dateRetourReel = d;
+	m_kilometreDepart = 0;
+	m_kilometreRetour = 0;
 }
 
 Location::Location(	Vehicule vehicule, 
-			CDate dateDepart, 
-			CDate dateRetourPrevu, 
-			CDate dateRetourReel, 
-			int kmDepart, 
-			int kmRetour) {
+					CDate dateDepart, 
+					CDate dateRetourPrevu, 
+					CDate dateRetourReel, 
+					int kmDepart, 
+					int kmRetour) {
 	m_vehicule = vehicule;
 	m_dateDepart = dateDepart;
 	m_dateRetourPrevu = dateRetourPrevu;
@@ -35,14 +42,34 @@ void Location::setVehicule(Vehicule veh) {
 	m_vehicule = veh;
 }
 
-CDate Location::getDateDepart() {
-	return m_dateDepart;
+CDate Location::getDate(char type) {
+	if (type =='d')
+		return m_dateDepart;
+	if (type == 'r')
+		return m_dateRetourReel;
+	if (type == 'p')
+		return m_dateRetourPrevu;
+	else {
+		// Pas propre. À transformer en Exception
+		cerr << "Type date inconnu : " << type << endl;
+		CDate res(0,0,0);
+		return res;
+	}
 }
 
-void Location::setDateDepart(CDate dateDepart) {
-	m_dateDepart = dateDepart;
+void Location::setDate(CDate date, char type) {
+	if (type == 'd')
+		m_dateDepart = date;
+	else if (type == 'r')
+		m_dateRetourReel = date;
+	else if (type == 'p')
+		m_dateRetourPrevu = date;
+	else {
+		cerr << "Type date inconnu : " << type << endl;
+	}
 }
 
+/*
 CDate Location::getDateRetourPrevue() {
 	return m_dateRetourPrevu;
 }
@@ -58,15 +85,29 @@ CDate Location::getDateRetourReelle() {
 void Location::setDateRetourReelle(CDate date) {
 	m_dateRetourReel = date;
 }
-
-int Location::getKmDepart() {
-	return m_kilometreDepart;
+*/
+int Location::getKilometrage(char type) {
+	if (type == 'd')
+		return m_kilometreDepart;
+	if (type == 'r')
+		return m_kilometreRetour;
+	else {
+		cerr << "Type kilométrage inconnu : " << type << endl;
+		return -1;
+	}	
 }
 
-void Location::setKmDepart(int km) {
-	m_kilometreDepart = km;
+void Location::setKilometrage(int km, char type) {
+	if (type == 'd')
+		m_kilometreDepart = km;
+	else if (type == 'r')
+		m_kilometreRetour = km;
+	else {
+		cerr << "Type kilométrage inconnu : " << type << endl;
+	}
 }
 
+/*
 int Location::getKmRetour() {
 	return m_kilometreRetour;
 }
@@ -74,6 +115,23 @@ int Location::getKmRetour() {
 void Location::setKmRetour(int kilom) {
 	m_kilometreRetour = kilom;
 }
-
+*/
 void Location::afficher() {
+	cout << "---------------------" << endl;
+	cout << "Véhicule" << endl;
+	m_vehicule.afficher();
+	cout << "Date de départ : ";
+	if (m_dateDepart == CDate(0,0,0))
+		cout << "Véhicule disponible" << endl;
+	else
+		m_dateDepart.afficher();
+	cout << "Date de retour prévu : ";
+	m_dateRetourPrevu.afficher();
+	cout << "Date de retour réel : ";
+	if (m_dateRetourReel == CDate(0,0,0))
+		cout << "Véhicule non restitué" << endl;
+	else
+		m_dateRetourReel.afficher();
+	cout << "Kilométrage départ : " << m_kilometreDepart << endl;
+	cout << "Kilométrage retour : " << m_kilometreRetour << endl;
 }
