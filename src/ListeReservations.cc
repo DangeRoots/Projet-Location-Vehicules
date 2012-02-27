922b5126a4f9c9a0746b589a87414d218dc079ef
Gilles <gilles.coulais@etu.u-bordeaux1.fr>
2012-02-27 16:56:54 +0100

    Revue et implémentation de l'algorithme d'ajout de réservation

--- src/ListeReservations.cc
+++ src/ListeReservations.cc
@@ -29,33 +29,38 @@ ListeReservations::ListeReservations() {
 ListeReservations::~ListeReservations() {
 }
 
+
 bool ListeReservations::ajouterReservation (Reservation res) {
-	bool ajouter = true;
 	m_listeReservationsI = m_listeReservations.begin();
-	
-	while (ajouter && m_listeReservationsI != m_listeReservations.end()) {
-		if (*m_listeReservationsI == res) {
-			if (!res.estDisponible(m_listeReservationsI->getDate('d'),m_listeReservationsI->getDate('r'))) {
+	bool ajouter = true;
+	// vérification des réservations existantes et chevauchement des périodes de location
+	while (ajouter && (m_listeReservationsI != m_listeReservations.end())) {
+		if (res.getVehicule() == m_listeReservationsI->getVehicule()) {
+			if (res.getDate('d') == m_listeReservationsI->getDate('d'))
 				ajouter = false;
-				cout << "Période indisponible " << ajouter << endl;
-			}
-		} else {
-			cout << "On continue" << endl;
+			else if (	res.getDate('d') > m_listeReservationsI->getDate('d') &&
+						(res.getDate('d') < m_listeReservationsI->getDate('r') ||
+						res.getDate('d') == m_listeReservationsI->getDate('r')))
+				ajouter = false;
+			else if (	res.getDate('d') < m_listeReservationsI->getDate('d') &&
+						(res.getDate('r') > m_listeReservationsI->getDate('d') ||
+						res.getDate('r') == m_listeReservationsI->getDate('d')))
+				ajouter = false;
+			else
+				m_listeReservationsI++;
+		} else
 			m_listeReservationsI++;
-		}
 	}
 	
-	// On ajoute la réservation dans la liste
-	cout << "J'ai terminé de vérifier la liste, je vais ajouter (ou pas)" << endl;
-	if (ajouter) {
-		m_listeReservations.push_back(res);
-		cout << "Ajout dans la liste" << endl;
+	// Ajout ou rejet de la réservation
+	if (!ajouter) {
+		cout << "Réservation impossible : pas de disponibilité aux dates demandées" << endl;
 	} else {
-		cout << "Pas d'ajout" << endl;
+		m_listeReservations.push_back(res);
 	}
-	// On confirme que l'ajout a été fait
 	return ajouter;
 }
+
 
 void ListeReservations::afficher(Parc p) {
 	for (m_listeReservationsI = m_listeReservations.begin();m_listeReservationsI != m_listeReservations.end();m_listeReservationsI++) {
@@ -67,5 +72,4 @@ void ListeReservations::sauvegarder(fstream &outFile) {
 	for (m_listeReservationsI = m_listeReservations.begin();m_listeReservationsI != m_listeReservations.end();m_listeReservationsI++) {
 		m_listeReservationsI->save(outFile);
 	}
-
 }
