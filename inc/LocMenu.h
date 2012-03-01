e9a09362a42a53d6625c8fa9d66682dd915b8a83
Gilles <gilles.coulais@etu.u-bordeaux1.fr>
2012-03-01 12:09:57 +0100

    LocMenu.h et LocMenu.cc vides

--- inc/LocMenu.h
+++ inc/LocMenu.h
@@ -7,7 +7,7 @@
 class LocMenu {
 
 	private:
-		vector<string> entrees;
+		vector<string> entrees;		
 		vector<string>::iterator menuI;
 
 	public:
@@ -18,20 +18,6 @@ class LocMenu {
 		int recupererValeur(string texte);
 
 		void traiter(int choix);
-
-	/*
-	int main() {
-		int choix(-1);
-		
-		while (choix != 0) {
-			afficherMenu();
-			choix = recupererValeur("Quel est votre choix : ");
-			traiter(choix);
-		}
-		
-		return 0;
-	}
-	*/
 
 };
 
